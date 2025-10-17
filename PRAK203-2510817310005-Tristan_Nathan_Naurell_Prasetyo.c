e5e8c2fa58379cd10782366a75a9d3eddf836fa3
natankriwill <aelfric310@gmail.com>
2025-10-17 15:53:59 +0800

    Update PRAK203-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c

--- PRAK203-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
+++ PRAK203-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
@@ -2,23 +2,10 @@
 
 int main(){
     double a, b, i, j, x, y;
-    double hasil1, hasil2, hasil3, hasil4, hasil5;
 
-    printf("Masukkan nilai a: ");
-    scanf("%lf", &a);
-    printf("Masukkan nilai b: ");
-    scanf("%lf", &b);
-    printf("Masukkan nilai c: ");
-    scanf("%lf", &i);
-    printf("Masukkan nilai d: ");
-    scanf("%lf", &j);
-    printf("Masukkan nilai e: ");
-    scanf("%lf", &x);
-    printf("Masukkan nilai f: ");
-    scanf("%lf", &y);
+    scanf("%lf %lf %lf %lf %lf %lf", &a, &b, &i, &j, &x, &y);
 
-    hasil1 = (a - b) * (i / j) - (x + y);
-    printf("Hasil 1: %.3f\n", hasil1);
+    printf("%.3f\n", (a - b) * (i / j) - (x + y));
 
     return 0;
 }
