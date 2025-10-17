91cd5291871df129adf77ca86c349ff590b2f3d4
natankriwill <aelfric310@gmail.com>
2025-10-17 15:57:05 +0800

    Update PRAK204-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py

--- PRAK204-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py
+++ PRAK204-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py
@@ -1,5 +1,16 @@
-r = float(input("Masukkan jari-jari: "))
-t = float(input("Masukkan tinggi: "))
+r = float(input())
+t = float(input())
+
+phi = 22/7
+volume = phi * r * r * t
+luas = 2 * phi * r * (r + t)
+keliling = 2 * phi * r
+
+print(f"volume = {volume:.2f}")
+print(f"luas = {luas:.2f}")
+print(f"keliling = {keliling:.2f}")
+
+r, t = map(float, input().split())
 
 phi = 22/7
 volume = phi * r * r * t
