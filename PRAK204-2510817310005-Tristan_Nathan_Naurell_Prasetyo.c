3fd6b308dab18672cb2af1c95f7a72f8ccb13472
natankriwill <aelfric310@gmail.com>
2025-10-17 15:56:24 +0800

    Update PRAK204-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
    Added duplicate input and calculation logic for radius and height.

--- PRAK204-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
+++ PRAK204-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
@@ -5,9 +5,7 @@ int main() {
     const double phi = 22.0 / 7.0;
     double volume, luas, keliling;
 
-    printf("Masukkan jari-jari: ");
     scanf("%lf", &r);
-    printf("Masukkan tinggi: ");
     scanf("%lf", &t);
 
     volume = phi * r * r * t;
@@ -17,5 +15,17 @@ int main() {
     printf("volume = %.2f\n", volume);
     printf("luas = %.2f\n", luas);
     printf("keliling = %.2f\n", keliling);
+
+    scanf("%lf", &r);
+    scanf("%lf", &t);
+
+    volume = phi * r * r * t;
+    luas = 2 * phi * r * (r + t);
+    keliling = 2 * phi * r;
+
+    printf("volume = %.2f\n", volume);
+    printf("luas = %.2f\n", luas);
+    printf("keliling = %.2f\n", keliling);
+    
     return 0;
 }
