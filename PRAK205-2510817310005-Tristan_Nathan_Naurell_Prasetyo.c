5983254f64e52237ff12adf0f13709c74823b9a0
natankriwill <aelfric310@gmail.com>
2025-10-17 15:57:37 +0800

    Update PRAK205-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
    Added duplicate calculations for 'alas', 'tinggi', 'keliling', and 'luas' based on new input values.

--- PRAK205-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
+++ PRAK205-2510817310005-Tristan_Nathan_Naurell_Prasetyo.c
@@ -3,9 +3,7 @@
 
 int main(){
     float a, b, alas, tinggi, keliling, luas;
-    printf("Masukkan nilai A: ");
     scanf("%f", &a);
-    printf("Masukkan nilai B: ");
     scanf("%f", &b);
 
     alas = sqrt(b*b - a*a);
@@ -17,4 +15,18 @@ int main(){
     printf("tinggi = %.0f cm\n", tinggi);
     printf("keliling = %.0f cm\n", keliling);
     printf("luas = %.0f cm^2\n", luas);
+
+    scanf("%f", &a);
+    scanf("%f", &b);
+
+    alas = sqrt(b*b - a*a);
+    tinggi = a;
+    keliling = a + b + alas;
+    luas = 0.5 * alas * tinggi;
+
+    printf("alas = %.0f cm\n", alas);
+    printf("tinggi = %.0f cm\n", tinggi);
+    printf("keliling = %.0f cm\n", keliling);
+    printf("luas = %.0f cm^2\n", luas);
+    return 0;
 }
