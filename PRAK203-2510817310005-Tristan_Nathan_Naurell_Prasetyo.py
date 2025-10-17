c792bcacd3c322aac324ae72cc6f5d2ff2f4f454
natankriwill <aelfric310@gmail.com>
2025-10-17 15:55:39 +0800

    Update PRAK203-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py
    Refactor input handling to use map for multiple values.

--- PRAK203-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py
+++ PRAK203-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py
@@ -1,9 +1,11 @@
-a = float(input("Masukkan nilai a: "))
-b = float(input("Masukkan nilai b: "))
-i = float(input("Masukkan nilai c: "))
-j = float(input("Masukkan nilai d: "))
-x = float(input("Masukkan nilai e: "))
-y = float(input("Masukkan nilai f: "))
+a, b, i, j, x, y = map(float, input().split())
 
 hasil = (a - b) * (i / j) - (x + y)
-print("{:.3f}".format(hasil))
+print(f"{hasil:.3f}")
+
+a, b = map(float, input().split())
+i, j = map(float, input().split())
+x, y = map(float, input().split())
+
+hasil = (a - b) * (i / j) - (x + y)
+print(f"{hasil:.3f}")
